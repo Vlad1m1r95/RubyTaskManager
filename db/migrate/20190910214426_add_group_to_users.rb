d3d6bad5a4aada0758c3fb9d203f55edeb159f81
Vladimir <vlad1im1r0195@gmail.com>
2019-09-22 04:50:04 +0300

    change migration

--- db/migrate/20190910214426_add_group_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :group, :string
-  end
-end
--- db/migrate/20190920002440_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :comments, :body, :description
-  end
-end
